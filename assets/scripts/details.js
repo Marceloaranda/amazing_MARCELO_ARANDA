6895556d983bc1d35fe84c4c70344ab71d8468a8
Marceloaranda <maranda.gm@gmail.com>
2023-03-15 11:03:02 -0300

    Modificaciones de acuerdo a Task3

---
+++ assets/scripts/details.js
@@ -0,0 +1,84 @@
+/* Obtenenos los datos de los parametros de la URL */
+
+let eventos = data.events
+
+const queryString = location.search
+let params = new URLSearchParams(queryString)
+
+let _id = params.get("_id")
+/* Buscar dato por id */
+let profile = eventos.find(evento => evento._id == _id)
+
+/* Renderizar profile */
+const container = document.getElementById("container-detail");
+let html = ""
+
+html += `
+<div class="detail-image-container">
+            <img src="${profile.image}" alt="${profile.name}">
+          </div>
+
+          <div class="details-text-container">
+            <h3>${profile.name}</h3>
+            <p>Date: <span>${profile.date}</span></p>
+            <p>Description: <span>${profile.description}</span></p>
+            <p>Category: <span>${profile.category}</span></p>
+            <p>Place: <span>${profile.place}</span></p>
+            <p>Capacity: <span>${profile.capacity}</span></p>
+            <p>Assistance: <span>${profile.assistance}</span></p>
+            <p>Price: <span>${profile.price}</span></p>
+          </div>
+    `
+container.innerHTML = html
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,15 +1,18 @@
+//  GENERA TARJETAS PARA HTML ******************************************
 
 let eventos = data.events;
 
 let containerCards = document.getElementById("cuerpo")
-console.log(containerCards);
 
-let stringCard = ""
+let generaCardsEvents = (dataArray) => {
 
-function generaCardsEvents(dataArray) {
-    for(evento of dataArray) {
-
-        stringCard += `
+  if(dataArray.length == 0){
+    containerCards.innerHTML = "<h2 class='display-1 fw-bolder'>There are no matching items!</h2>"
+    return
+}
+  let stringCard = ""
+  dataArray.forEach(evento => {
+    stringCard += `
 
         <div class="card" style="width: 18rem;">
               <img src=${evento.image}class="card-img-top" alt="cinema">
@@ -19,14 +22,90 @@ function generaCardsEvents(dataArray) {
                 </p>
                 <div class="d-flex">
                   <p>Price $ ${evento.price}</p>
-                  <a href="./details.html" class="btn btn-primary">Go somewhere</a>
+                  <a href="./details.html?_id=${evento._id}" class="btn btn-primary">Details</a>
                 </div>
               </div>
             </div> 
         `
-    }
+  })
+
+  containerCards.innerHTML = stringCard
 }
 
 generaCardsEvents(eventos)
 
-containerCards.innerHTML = stringCard
+// *********************************************************************************************
+
+  
+
+/// GENERA CATEGORIAS PARA HTML*********************************************************************
+
+let categorias = eventos.map((evento) => evento.category)
+ //console.log(categorias)
+
+let categorys = categorias.filter((cat, indice) => {
+   return categorias.indexOf(cat) === indice;
+}
+)
+
+// console.log(categorys)
+
+let containerCategory = document.getElementById("catlist")
+//console.log(containerCategory)
+
+let generaCat = (dataArray) => {
+  let stringCat = ""
+  dataArray.forEach(cat => {
+                                                                                      
+    stringCat += `
+
+    <label class="category"><input name="position1" type="checkbox" value="${cat}">${cat}</label>
+
+        `
+  })
+
+  containerCategory.innerHTML = stringCat
+}
+
+generaCat(categorys)
+
+//**************************************************************************************************** */
+
+
+// FUNCIONES DE FILTROS
+
+
+function filtroCruzado (){
+  let arrayTextoIngresado = filtrarXTexto(eventos,input.value)
+  let arrayFiltrado = filtrarXCat(arrayTextoIngresado)
+  generaCardsEvents(arrayFiltrado)
+}
+
+
+
+
+/// FILTOR POR CATEGORIA
+
+const divCheckBoxs = document.querySelector('#catlist')
+divCheckBoxs.addEventListener('change',filtroCruzado)
+
+function filtrarXCat(lista){
+  let radios = Array.from(document.querySelectorAll("input[type='checkbox']"))
+  let radioCheckeado = radios.filter((radio) => radio.checked).map((radio) => radio.value)
+  if (radioCheckeado.length === 0){
+    return lista
+  } else {
+  let eventFiltrado = lista.filter((elemento) => radioCheckeado.includes(elemento.category))
+  return eventFiltrado;
+}
+}
+
+// FILTO POR TEXTO 
+
+const input = document.querySelector('input.form-control')
+input.addEventListener('input',filtroCruzado)
+
+function filtrarXTexto(array, texto){
+  let arrayTextoIngresado= array.filter(evento => evento.name.toLowerCase().includes(texto.toLowerCase()))
+  return arrayTextoIngresado
+}
